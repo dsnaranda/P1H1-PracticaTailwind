cb9ef38e4407dc5abaa3b8aca8a6c8cd4389e1ea
MegaLodias <derikaranda@gmail.com>
2024-10-27 14:56:11 -0500

    Creación de la conexión a Atlas

--- src/service/dbConex.js
+++ src/service/dbConex.js
@@ -1,5 +1,6 @@
 import { MongoClient } from "mongodb";
 
+//Cambios para MongoAtlas
 const getConenection = async () => {
     try {
         const mongoUrl = "mongodb://localhost:27017/T1H1DataBase_Tienda";
